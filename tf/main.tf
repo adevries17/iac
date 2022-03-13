6b345915b5a5cd2ea308036a40a88acfd349ee9c
adevries17 <ajdevries530@gmail.com>
2022-03-12 21:46:22 -0600

    initial upload

---
+++ tf/main.tf
@@ -0,0 +1,36 @@
+terraform {
+    required_providers {
+        proxmox = {
+            source = "telmate/proxmox"
+            version = "2.9.4"
+        }
+    }
+}
+provider "proxmox" {
+    pm_api_url          = "https://vmtoog.turtlesnet.dev:8006/api2/json"
+    pm_api_token_id     = var.pm_api_token_id
+    pm_api_token_secret = var.pm_api_token_secret
+}
+resource "proxmox_lxc" "rockmc-0" {
+    count=1
+    target_node="vmtoog"
+    hostname="gunnermc"
+    ostemplate=var.rockt
+    unprivileged=true
+    onboot=true
+    start=true
+    vmid=150
+    cores=4
+    memory=12288
+    rootfs {
+        storage=var.lvmt
+        size="8G"
+    }
+    network {
+        name="eth0"
+        bridge="vmbr0"
+        ip="192.168.17.150/24"
+        gw="192.168.17.1"
+    }
+    ssh_public_keys=var.sshkey
+}
---
+++ tf/variables.tf
@@ -0,0 +1,21 @@
+variable "archtmpl" {
+    default="local:vztmpl/archlinux-base_20201116-1_amd64.tar.gz"
+}
+variable "debtmpl" {
+    default="local:vztmpl/debian-11-standard_11.0-1_amd64.tar.gz"
+}
+variable "rockt" {
+    default="local:vztmpl/rockylinux-8-default_20210929_amd64.tar.xz"
+}
+variable "lvmt" {
+    default="local-lvm"
+}
+variable "sshkey" {
+    sensitive=true
+}
+variable "pm_api_token_id" {
+    sensitive=true
+}
+variable "pm_api_token_secret" {
+    sensitive=true
+}
