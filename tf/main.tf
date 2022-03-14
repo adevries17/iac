1cafa26aa0473d46f74711c6d326453aa4a84e5a
adevries17 <ajdevries530@gmail.com>
2022-03-13 19:45:27 -0500

    update api url

--- tf/main.tf
+++ tf/main.tf
@@ -7,7 +7,7 @@ terraform {
     }
 }
 provider "proxmox" {
-    pm_api_url          = "https://vmtoog.turtlesnet.dev:8006/api2/json"
+    pm_api_url          = "https://tnpve1.turtlesnet.dev:8006/api2/json"
     pm_api_token_id     = var.pm_api_token_id
     pm_api_token_secret = var.pm_api_token_secret
 }
