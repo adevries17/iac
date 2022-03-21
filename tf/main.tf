de95f8e347b4d0776efdbe2f0a85b9f1761a5997
adevries17 <ajdevries530@gmail.com>
2022-03-21 18:39:43 -0500

    add valheim instance

--- tf/main.tf
+++ tf/main.tf
@@ -11,7 +11,6 @@ provider "proxmox" {
     pm_api_token_id     = var.pm_api_token_id
     pm_api_token_secret = var.pm_api_token_secret
 }
-
 # resources
 resource "proxmox_lxc" "factorygame" {
     count           = 2
@@ -61,11 +60,34 @@ resource "proxmox_lxc" "minecraft" {
         storage     = var.lvmt
     }
 }
+resource "proxmox_lxc" "valheim" {
+    count           = 1
+    cores           = 2
+    hostname        = "valheim-${count.index+1}"
+    memory          = 4096
+    onboot          = true
+    ostemplate      = var.rockt
+    protection      = false
+    ssh_public_keys = var.sshkey
+    start           = true
+    swap            = 512
+    target_node     = "tnpve1"
+    unprivileged    = true
+    network {
+        bridge      = "vmbr0"
+        gw          = "192.168.17.1"
+        ip          = "dhcp"
+        name        = "eth0"
+    }
+    rootfs {
+        size        = "8G"
+        storage     = var.lvmt
+    }
+}
 resource "local_file" "ansible_inventory" {
-    content = templatefile("${abspath(path.root)}/templates/ansible-inventory.tpl", { factorygame=[for host in proxmox_lxc.factorygame.*: "${host.hostname}.turtlesnet.dev"], minecraft=[for host in proxmox_lxc.minecraft.*: "${host.hostname}.turtlesnet.dev"] })
+    content = templatefile("${abspath(path.root)}/templates/ansible-inventory.tpl", { factorygame=[for host in proxmox_lxc.factorygame.*: "${host.hostname}"], minecraft=[for host in proxmox_lxc.minecraft.*: "${host.hostname}"], valheim=[for host in proxmox_lxc.valheim.*: "${host.hostname}"] })
     filename = "${dirname(abspath(path.root))}/ansible/inventory.ini"
 }
-
 /*resource "proxmox_lxc" "rockmc-0" {
     count           = 1
     cores           = 8
